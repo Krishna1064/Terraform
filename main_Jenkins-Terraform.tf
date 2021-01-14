1092137c91c5844f805353cda7002c1400dea667
Krishna1064 <65502533+Krishna1064@users.noreply.github.com>
2021-01-13 19:35:22 -0500

    Update main_Jenkins-Terraform.tf

--- main_Jenkins-Terraform.tf
+++ main_Jenkins-Terraform.tf
@@ -2,14 +2,14 @@ provider "aws" {
     region = "us-east-2"
 }
 
-
-resource "aws_instance" "ec2test" {
+resource "aws_instance" "k8scluster" {
+  count = 2
   ami = "ami-0a91cd140a1fc148a"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["Jenkins-SG"]
   key_name = "TestEKS"
   iam_instance_profile = "EC2fullaccessRole"
-  tags = {Name = "Jenkins-Server"}
+  tags = {Name = "k8scluster-${count.index}"}
   user_data = <<-EOF
    #!/bin/bash
    sudo apt update
@@ -19,25 +19,29 @@ resource "aws_instance" "ec2test" {
     /etc/apt/sources.list.d/jenkins.list'
    sudo apt-get update
    sudo apt-get install jenkins -y
-   sudo systemctl status jenkins.service
    sudo systemctl start jenkins.service
    sudo systemctl enable jenkins.service
-   sudo ufw allow 8080
-   sudo ufw allow OpenSSH
-   sudo ufw enable
-   sudo ufw status
    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
    sudo apt-get update && sudo apt-get install terraform
    sudo apt install git -y
    sudo apt install apache2-utils -y
    EOF
-  }
 }
 
+resource "local_file" "inventory" {
+ filename = "./inventory/hosts.ini"
+ content = <<EOF
+[k8scluster]
+${aws_instance.k8scluster[0].public_ip}
+${aws_instance.k8scluster[1].public_ip}
+EOF
+}
+
+/*
 output "PublicIP" {
-value = aws_instance.ec2test.public_ip
+value = aws_instance.k8scluster.public_ip
 #  value = "${formatlist("%v", aws_instance.cZServers.*.public_ip)}"
 }
-
+*/
 
